2e72b0f386bd43743b9b69f24a8117336f5f68fa
Alex Sytchev <sytchevalexander@gmail.com>
2021-11-28 19:49:46 +0100

    Updated armor

--- Scenes/Items/Armors/Armor.gd
+++ Scenes/Items/Armors/Armor.gd
@@ -1,6 +1,13 @@
 extends Node2D
 class_name Armor
 
+export var multipliers : Dictionary = {}
 
-func modify_damage(damage : float):
-	return damage
+func modify(modifiers : Dictionary):
+	modifiers = modifiers.duplicate()
+	
+	for key in modifiers:
+		if multipliers.has(key):
+			modifiers[key] = modifiers[key] * multipliers[key]
+	
+	return modifiers
