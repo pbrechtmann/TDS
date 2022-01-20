4102c7b28464837f3aefb3fb999a9861c8edafb2
unknown <44525951+pbrechtmann@users.noreply.github.com>
2022-01-20 09:15:29 +0100

    added bounce functionality to projectiles

--- Scenes/Items/Projectiles/Projectile.gd
+++ Scenes/Items/Projectiles/Projectile.gd
@@ -3,10 +3,14 @@ class_name Projectile
 
 var direction : Vector2 = Vector2.ZERO
 
-var speed = 1000
-var pierce = 0
+var speed : float = 1000
+
+var pierce : int = 0
+var bounce : int = 0
 
 var modifiers : Dictionary = {}
+
+onready var collision_ray : RayCast2D = $RayCast2D
 
 
 func init(muzzle_transform : Transform2D, dir : Vector2, modifiers : Dictionary) -> void:
@@ -15,13 +19,14 @@ func init(muzzle_transform : Transform2D, dir : Vector2, modifiers : Dictionary)
 	global_transform = muzzle_transform
 	
 	self.modifiers = modifiers
-	
 	for key in modifiers:
 		match key:
 			"speed":
 				speed = modifiers[key]
 			"pierce":
 				pierce = modifiers[key]
+			"bounce":
+				bounce = modifiers[key]
 
 
 func _physics_process(delta) -> void:
@@ -35,12 +40,23 @@ func handle_pierce() -> bool:
 	return true
 
 
+func handle_bounce() -> bool:
+	if bounce > 0:
+		bounce -= 1
+		direction = direction.bounce(collision_ray.get_collision_normal().normalized())
+		rotation = direction.angle()
+		return false
+	return true
+
+
 func _on_Area2D_body_entered(body : PhysicsBody2D) -> void:
 	var done = true
 	
 	if body is Entity:
 		body.get_damage(modifiers)
 		done = handle_pierce()
+	else:
+		done = handle_bounce()
 		
 	if done:
 		queue_free()
