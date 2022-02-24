c3f6c9608fd92a6e439e6f3a71319770cf92c5b9
pbrechtmann <44525951+pbrechtmann@users.noreply.github.com>
2022-02-24 15:29:48 +0100

    Basic armor fix

--- Scenes/Items/Upgrades/Modules/Armor/UpgradeArmor.gd
+++ Scenes/Items/Upgrades/Modules/Armor/UpgradeArmor.gd
@@ -4,7 +4,7 @@ var improvement_multipliers : float = 0.05
 
 func apply_upgrade(to : Node2D) -> void:
 	if to is Armor:
-		if to.has("damage"):
+		if to.multipliers.has("damage"):
 			to.multipliers["damage"] += to.multipliers["damage"] * improvement_multipliers
 		else:
 			to.multipliers["damage"] = improvement_multipliers
